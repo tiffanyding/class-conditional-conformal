3f7077854d1e88526e39d13c45111b507c34318a
Tiffany <tifding@gmail.com>
2025-08-14 09:13:05 -0700

    [moved Google Drive location]

--- download_data.sh
+++ download_data.sh
@@ -1,7 +1,8 @@
 mkdir data
 cd data
-gdown 1Ax-mP7PJXHEWo4TbUMru5vVjGjmpK5gE # ImageNet (4.62 GB)
-gdown 12huZJjuubElkMKB9y05M1Bz_VYH3E0T_ # CIFAR-100 (0.01 GB)
-gdown 1LtEDNYiru2hJIOlJFKfa2rS3z_oHcvC5 # Places365 (0.54 GB)
-gdown 1D8H_vgzRc66cHAukyOgT0GUm5_u8dX5x # iNaturalist (6.72 GB)
+gdown 1AQjUn3m010N_i6-sfD690W7mZq2RTwJz # ImageNet (4.62 GB)
+gdown 1yXD9XqBxEJnJxcfnnduNK6nHHUU3iX_6 # CIFAR-100 (0.01 GB)
+gdown 119k7PE1l72fg5Rpez5brIOn28BwClqv2 # Places365 (0.54 GB)
+gdown 1BUlQZhS_5x2LJpyxCGI1IkmRrkvmRD88 # iNaturalist (6.72 GB)
 cd .. 
+
